a20ca6c7f771cd2109c4b71c2c7a50c616229fa4
testnameaa <159999719+testnameaa@users.noreply.github.com>
2026-08-19 10:54:20 +0900

    Update DbSelectTasklet.java

--- doma2test2/main/java/com/example/doma2test2/tasklet/DbSelectTasklet.java
+++ doma2test2/main/java/com/example/doma2test2/tasklet/DbSelectTasklet.java
@@ -29,14 +29,15 @@ public class DbSelectTasklet implements Tasklet {
         List<JyuOrgDto> stepDtoPut = new ArrayList<>();
 
         // 今回は学習用という事で抽出条件は固定値とする
-        Date startDate = Date.valueOf("2025-07-28");
-        Date endDate = Date.valueOf("2025-07-28");
-        String toriCd = "AB000011";
+        Date startDate = Date.valueOf("2025-06-27");
+        Date endDate = Date.valueOf("2025-07-02");
+        String toriCd = "00000002";
         List<JyuOrg> listJyuOrg = jyuOrgDaoCustom.selectTestIF(toriCd, startDate, endDate);
+        
         // 練習問題13(回答例)
-//        Date startDate = Date.valueOf("2025-06-15");
-//        Date endDate = Date.valueOf("2025-06-17");
-//        String braCd = "00000012";
+//        Date startDate = Date.valueOf("2025-07-28");
+//        Date endDate = Date.valueOf("2025-07-28");
+//        String braCd = "AB000012";
 //        List<JyuOrg> listJyuOrg = jyuOrgDaoCustom.selectTestIF(braCd, startDate, endDate);
         
         for (JyuOrg jyuOrg : listJyuOrg) {
